18b91432fc4fc056558e046a329a50551c4c904b
rybak194 <rybak194@gmail.com>
2018-04-03 09:04:09 +0200

    v2

--- bulk-satellite-update.sh
+++ bulk-satellite-update.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-bash update-root.bash pw.esn.pl esn_pw || echo -e "\e[31mUpdate failed\e[0m"
-bash update-root.bash azure-test.esn.pl esn_azure || echo -e "\e[31mUpdate failed\e[0m"
-bash update-root.bash pwr.esn.pl esn_pwr || echo -e "\e[31mUpdate failed\e[0m"
-bash update-root.bash agh.esn.pl esn_agh || echo -e "\e[31mUpdate failed\e[0m"
+declare -a SITES_ARRAY=(sekcja1.esn.pl
+sekcja2.esn.pl
+sekcja3.esn.pl)
+
+for SITE in "${SITES_ARRAY[@]}"
+do
+	if bash satellite-update.sh $SITE; then
+		echo -e "\e[92m$SITE - update finished successfully \e[0m"
+	else
+	   echo -e "\e[31m$SITE - update failed\e[0m"
+	fi
+done
--- satellite-update.sh
+++ satellite-update.sh
@@ -1,41 +1,44 @@
 #!/bin/sh
 
 SITE=$1
-USER=$2
 
+PACKAGE_PATH="/root/update-test/latest_package.tgz"
 SITE_DIR="/var/www/$SITE/web"
+DIR_OWNER=$(stat -c '%U' $SITE_DIR)
 
-echo -e "\e[93m********************** $SITE_DIR | $USER *******************************\e[0m"
+echo -e "\e[93m********************** $SITE_DIR | $DIR_OWNER *******************************\e[0m"
+
+if [ ! -d "$SITE_DIR" ]; then
+  echo -e "\e[31m$SITE_DIR - does not exist\e[0m" && exit 1
+fi
+
+if [ ! -f "$PACKAGE_PATH" ]; then
+  echo -e "\e[31m$PACKAGE_PATH - does not exist\e[0m" && exit 1
+fi
 
 cd $SITE_DIR
 drush vset --exact maintenance_mode 1
 drush cache-clear all
 echo "Enabled maintenance mode"
 
-echo "Removing files..."
-rm -rf profiles themes scripts modules misc includes && rm -f *
+echo "Purging site directory: $SITE_DIR ..."
+rm -rf profiles themes scripts modules misc includes
+rm -f .* 2> /dev/null
 
-echo "Copying satellite package files..."
-cp /root/update-test/latest_package.tgz $SITE_DIR
+echo "Unpacking satellite package $PACKAGE_PATH in $SITE_DIR ..."
+tar xfz $PACKAGE_PATH -C $SITE_DIR --strip-components=1
 
-echo "Unpacking satellite package..."
-tar xfz latest_package.tgz --strip-components=1
-
-echo "Removing satellite package..."
-rm -f latest_package.tgz
+echo "Changing web directory owner to $DIR_OWNER ..."
+chown -R $DIR_OWNER $SITE_DIR
 
 echo "Applying database migrations..."
 if drush updatedb -y; then
    echo "Database migrated"
 else
-   echo -e "\e[31m$SITE - database migration failed\e[0m" && exit 1
+   echo -e "\e[31mDatabase migration failed\e[0m" && exit 1
 fi
-
-echo "Changing web directory ownership..."
-chown -R $USER $SITE_DIR
 
 drush vset --exact maintenance_mode 0
 drush cache-clear all
 echo "Disabled maintenance mode"
 
-echo -e "\e[92m$SITE - update finished successfully \e[0m"
