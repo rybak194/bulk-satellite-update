156051ac8d0aaa20b61688b0b3cd2e6bd7d4f768
rybak194 <rybak194@gmail.com>
2019-04-15 11:46:24 +0200

    Update bulk-satellite-update.sh

--- bulk-satellite-update.sh
+++ bulk-satellite-update.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # example usages:
 # bash bulk-satellite-update.sh
+# bash bulk-satellite-update.sh 2>&1 | tee bulk-satellite-update.log // writes output to log file
+# bash bulk-satellite-update.sh 2>&1 | tee >(ts "%d-%m-%y %H:%M:%S" > bulk-satellite-update.log) //writes output to log file with timestamp
 
 declare -a SITES_ARRAY=(sekcja1.esn.pl
 sekcja2.esn.pl
